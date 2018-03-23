4feec15bbfa7fd5a34f157fcef9f75ddb7fd1a10
AnttiSor <antti.sorvisto@student.laurea.fi>
2018-03-23 18:03:46 +0200

    Initial Commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-tabs',
+  templateUrl: 'tabs.html',
+})
+export class TabsPage {
+  tab1Root='HomePage';
+  tab2Root='AboutPage';
+  tab3Root='ActivitiesPage';
+  tab4Root='ContactInformationPage';
+  tab5Root='ExtraCurriculumPage';
+  tab6Root='TargetedCvPage';
+  myIndex: number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.myIndex = navParams.data.tabIndex || 0;
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TabsPage');
+  }
+
+} 
